a96103b7298f64237beaff8a9fded72097549686
Eren <86615310+erent8@users.noreply.github.com>
2023-08-08 16:13:39 +0300

    Update main.py

--- MeteologhySystem/main.py
+++ MeteologhySystem/main.py
@@ -1,7 +1,8 @@
 import requests
+import datetime
 
 API_KEY = "823bcb1f8964ccdb90c1fe8103fb981b"
-BASE_URL = "http://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={823bcb1f8964ccdb90c1fe8103fb981b}"
+BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?"
 
 SEHIR_ISMI = input("Şehir ismi giriniz...")
 
@@ -11,18 +12,30 @@ gelen_veri = requests.get(URL)
 gelen_veri_JSON = gelen_veri.json()
 
 if gelen_veri_JSON['cod'] != "404":
-    temp_kelvin = gelen_veri_JSON["main"]["temp"]
-    description = gelen_veri_JSON["weather"][0]["description"]
-    pressure = gelen_veri_JSON["main"]["pressure"]
-    country = gelen_veri_JSON["sys"]["country"]
+    forecasts = gelen_veri_JSON["list"]  # Hava durumu tahminlerini içeren liste
 
-    # Kelvin'i Celsius'a dönüştürme: °C = K - 273.15
-    temp_celsius = temp_kelvin - 273.15
+    for forecast in forecasts:
+        timestamp = forecast["dt"]  # Unix zaman damgası
+        temp_kelvin = forecast["main"]["temp"]
+        description = forecast["weather"][0]["description"]
+        pressure = forecast["main"]["pressure"]
+        country = gelen_veri_JSON["city"]["country"]
 
-    print("Sıcaklık: {:.2f} °C".format(temp_celsius))
-    print("Hava Durumu:", description)
-    print("Basınç:", pressure, "hPa")
-    print("Ülke:", country)
+        # Unix zaman damgasını datetime nesnesine dönüştürme
+        tarih = datetime.datetime.fromtimestamp(timestamp)
+        gun = tarih.strftime("%A")  # Gün adını almak
+        tarih_str = tarih.strftime("%d/%m/%Y %H:%M")  # Tarih ve saat bilgisini almak
+
+        # Kelvin'i Celsius'a dönüştürme: °C = K - 273.15
+        temp_celsius = temp_kelvin - 273.15
+
+        print("Date Time:", tarih_str)
+        print("Day:", gun)
+        print("Temperature: {:.2f} °C".format(temp_celsius))
+        print("Weather:", description)
+        print("Pressure:", pressure, "hPa")
+        print("Country:", country)
+        print("------------------------")
 
 else:
     print("Geçersiz şehir adı...")
